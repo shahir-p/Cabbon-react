98e984181814f2ea07849fb619bd28e35117109d
Shahir <shahirp70@gmail.com>
2025-03-19 16:32:09 +0530

    username added

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import servicebg from '../assets/service bg.png'
 import paymentbg from '../assets/paymentbg.png'
 import todaybg from '../assets/today bg.png'
+import profile from '../assets/profile.png'
 
 
 
@@ -11,25 +12,29 @@ const HomePage = ({ height, width }) => {
 
     return (
         <>
-            <div className='home d-flex flex-column ' style={{ width: `${width}px`, height: `${height}px`, }}>
-                <div className='today ' style={{
-                    height: `${height * 0.2}px`, margin: "10px", marginTop: `${height * 0.1 + 10}px`, borderRadius: "10px", border: "1px solid", padding: "10px", backgroundImage: `url(${todaybg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: "right"
+            <div className='home d-flex flex-column ' style={{ width: `${width}px`, height: `${height}px`,paddingLeft:"10px",paddingRight:"10px" }}>
+                <div style={{ marginTop: `${height * 0.1 + 15}px`,}}>
+                    {/* <img src={profile} alt="" width={"50px"} height={"0px"} /> */}
+                    <span style={{ paddingLeft: "10px", fontSize: "18px" }}>Welcome Shahir ,</span>
+
+                </div>                <div className='today ' style={{
+                    height: `${height * 0.18}px`, margin: "10px", borderRadius: "10px", border: "1px solid", padding: "10px", backgroundImage: `url(${todaybg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: "right"
                 }}>
-                    <div className=' d-flex justify-content-between'> <span style={{ fontSize: "18px", fontWeight: "600" }}>TODAY</span>
-                        <span style={{ color: "rgba(50, 168, 82)", fontSize: "16px", fontWeight: "500" }}>08:30 am</span>
+                    <div className=' d-flex justify-content-between'> <span style={{ fontSize: "18px", fontWeight: "500" }}>TODAY</span>
+                        <span style={{ color: "rgba(50, 168, 82)", fontSize: "16px", fontWeight: "400" }}>08:30 am</span>
                     </div>
-                    <div className='d-flex justify-content-center mt-4'><span style={{ fontSize: "20px", fontWeight: "600" }}>Qatar auditoruim</span></div>
+                    <div className='d-flex justify-content-center mt-4'><span style={{ fontSize: "20px", fontWeight: "500" }}>Qatar auditoruim</span></div>
                 </div>
                 <span style={{ marginLeft: "10px", fontSize: "18px", fontWeight: "500" }}>Overview</span>
                 <div className='service ' style={{ height: `${height * 0.2}px`, margin: "10px", marginTop: "10px", borderRadius: "10px", border: "1px solid", padding: "10px", backgroundImage: `url(${servicebg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: "right" }}>
-                    <div className=''> <span style={{ fontSize: "18px", fontWeight: "600" }}>Service</span>
+                    <div className=''> <span style={{ fontSize: "18px", fontWeight: "500" }}>Service</span>
                         <span style={{ color: "rgba(50, 168, 82)", fontSize: "16px", fontWeight: "500" }}></span>
                     </div>
                     <div className='d-flex justify-content-start mt-2'><span style={{ fontSize: "20px", fontWeight: "500" }}>25</span></div>
                 </div>
 
                 <div className='Payment ' style={{ height: `${height * 0.2}px`, margin: "10px", marginTop: "10px", borderRadius: "10px", border: "1px solid", padding: "10px", backgroundImage: `url(${paymentbg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: "right" }}>
-                    <div className=' d-flex justify-content-between'> <span style={{ fontSize: "18px", fontWeight: "600" }}>Earnings</span>
+                    <div className=' d-flex justify-content-between'> <span style={{ fontSize: "18px", fontWeight: "500" }}>Earnings</span>
                         <span style={{ color: "rgba(50, 168, 82)", fontSize: "16px", fontWeight: "500" }}></span>
                     </div>
                     <div className='d-flex justify-content-start mt-2'><span style={{ fontSize: "20px", fontWeight: "500" }}>2500</span></div>
