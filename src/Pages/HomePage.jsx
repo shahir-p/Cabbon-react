1cd6ad678a45e972dc9e70b78fbb1eb46a4ad794
Shahir <shahirp70@gmail.com>
2025-03-19 14:54:12 +0530

    modal added

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,16 +3,17 @@ import { useState, useEffect } from 'react';
 import servicebg from '../assets/service bg.png'
 import paymentbg from '../assets/paymentbg.png'
 import todaybg from '../assets/today bg.png'
+import { Link } from 'react-router-dom';
 
 
 
 const HomePage = ({ height, width }) => {
-  
+
     return (
         <>
             <div className='home d-flex flex-column ' style={{ width: `${width}px`, height: `${height}px`, }}>
                 <div className='today ' style={{
-                    height: `${height * 0.2}px`, margin: "10px", marginTop: `${height * 0.1 + 10}px`, borderRadius: "10px", border: "1px solid", padding: "10px",backgroundImage: `url(${todaybg})`,backgroundSize: 'cover',backgroundRepeat: 'no-repeat',backgroundPosition:"right"
+                    height: `${height * 0.2}px`, margin: "10px", marginTop: `${height * 0.1 + 10}px`, borderRadius: "10px", border: "1px solid", padding: "10px", backgroundImage: `url(${todaybg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: "right"
                 }}>
                     <div className=' d-flex justify-content-between'> <span style={{ fontSize: "18px", fontWeight: "600" }}>TODAY</span>
                         <span style={{ color: "rgba(50, 168, 82)", fontSize: "16px", fontWeight: "500" }}>08:30 am</span>
@@ -20,18 +21,20 @@ const HomePage = ({ height, width }) => {
                     <div className='d-flex justify-content-center mt-4'><span style={{ fontSize: "20px", fontWeight: "600" }}>Qatar auditoruim</span></div>
                 </div>
                 <span style={{ marginLeft: "10px", fontSize: "18px", fontWeight: "500" }}>Overview</span>
-                <div className='service ' style={{ height: `${height * 0.2}px`, margin: "10px", marginTop: "10px", borderRadius: "10px", border: "1px solid", padding: "10px" , backgroundImage: `url(${servicebg})`,backgroundSize: 'cover',backgroundRepeat: 'no-repeat',backgroundPosition:"right"}}>
+                <div className='service ' style={{ height: `${height * 0.2}px`, margin: "10px", marginTop: "10px", borderRadius: "10px", border: "1px solid", padding: "10px", backgroundImage: `url(${servicebg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: "right" }}>
                     <div className=''> <span style={{ fontSize: "18px", fontWeight: "600" }}>Service</span>
                         <span style={{ color: "rgba(50, 168, 82)", fontSize: "16px", fontWeight: "500" }}></span>
                     </div>
                     <div className='d-flex justify-content-start mt-2'><span style={{ fontSize: "20px", fontWeight: "500" }}>25</span></div>
                 </div>
-                <div className='Payment ' style={{ height: `${height * 0.2}px`, margin: "10px", marginTop: "10px", borderRadius: "10px", border: "1px solid", padding: "10px" ,backgroundImage: `url(${paymentbg})`,backgroundSize: 'cover',backgroundRepeat: 'no-repeat',backgroundPosition:"right"}}>
+
+                <div className='Payment ' style={{ height: `${height * 0.2}px`, margin: "10px", marginTop: "10px", borderRadius: "10px", border: "1px solid", padding: "10px", backgroundImage: `url(${paymentbg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: "right" }}>
                     <div className=' d-flex justify-content-between'> <span style={{ fontSize: "18px", fontWeight: "600" }}>Earnings</span>
                         <span style={{ color: "rgba(50, 168, 82)", fontSize: "16px", fontWeight: "500" }}></span>
                     </div>
                     <div className='d-flex justify-content-start mt-2'><span style={{ fontSize: "20px", fontWeight: "500" }}>2500</span></div>
                 </div>
+
             </div>
         </>
 
--- src/Pages/ServicePage.jsx
+++ src/Pages/ServicePage.jsx
@@ -1,20 +1,69 @@
 import React from 'react'
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 
 const ServicePage = ({ height, width }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <div className='home d-flex flex-column ' style={{ width: `${width}px`, marginTop: `${height * 0.1 + 10}px`, padding: "5px" }}>
       <h5>Services</h5>
-      <div className='list d-flex justify-content-evenly align-items-center mb-2' style={{ height: `${height * 0.07}px`, width: `${width - 10}px`, border: "1px solid", borderRadius: "10px",fontSize:"13px" }}>
+      <div className='list d-flex justify-content-evenly align-items-center mb-2' style={{ height: `${height * 0.07}px`, width: `${width - 10}px`, border: "1px solid", borderRadius: "10px", fontSize: "13px" }} onClick={handleShow}>
         <span>1.</span>
         <span>10-02-25</span>
         <span>Qatar Auditoruim</span>
-        <span style={{color: "rgba(50, 168, 82)",}}>08:30 am</span>
-        <span style={{color: "rgba(191, 48, 48)",}}>04:30 pm</span>
+        <span style={{ color: "rgba(50, 168, 82)", }}>08:30 am</span>
+        <span style={{ color: "rgba(191, 48, 48)", }}>04:30 pm</span>
         <span><i class="fa-solid fa-triangle-exclamation text-warning" ></i></span>
       </div>
-     
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Service Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className='d-flex'>
+         <div>
+            <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
+              <li>Date </li>
+              <li>Venue </li>
+              <li>Entry </li>
+              <li>Exit </li>
+              <li>Fair </li>
+              <li>Expense </li>
+              <li>Others </li>
+              <li>Fine </li>
+              <li style={{fontSize:"18px",fontWeight:"600"}}>Total </li>
+            </ul>
+  
+         </div>
+         <div className='ms-2'>
+            <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
+              <li>: 10-02-2025</li>
+              <li>: Qatar Auditorium</li>
+              <li style={{ color: "rgba(50, 168, 82)", }}>: 08:30 am</li>
+              <li style={{ color: "rgba(191, 48, 48)", }}>: 04:30 pm</li>
+              <li>: 480</li>
+              <li>: 0</li>
+              <li>: 0</li>
+              <li>: <span>Reason</span> | <span>- 0</span></li>
+              <li style={{fontSize:"18px",fontWeight:"600"}}>: 480</li>
+            </ul>
+  
+         </div>
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+
+        </Modal.Footer>
+      </Modal>
+
     </div>
   )
 }
